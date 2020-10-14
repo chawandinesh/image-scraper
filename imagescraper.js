3cc738c6270fb6d5c6bea139bf214f68ca6b18d3
dinesh <chavandinesh53@gmail.com>
2020-10-15 01:06:53 +0530

    changes in all files

---
+++ imagescraper.js
@@ -0,0 +1,43 @@
+const puppeteer = require("puppeteer");
+const express = require("express");
+const app = express();
+const port = 3006;
+const cherio = require("cheerio");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+app.use(bodyParser());
+app.use(cors());
+app.post("/api/data",  async(req, res) => {
+  const { url } = req.query;
+  if (!url) {
+    res.status(400).send("Bad request: 'url' param is missing!");
+    return;
+  }
+  try {
+    const html =  await getPageHTML(url);
+    const $ = cherio.load(html);
+    let arrayData = [];
+    $("img").each((index, image) => {
+      var img = $(image).attr("src");
+      let Links;
+      if(img){
+        Links = img
+      }
+      arrayData.push(Links);
+    });
+    res.status(200).send(arrayData);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+const getPageHTML = async (pageUrl) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(pageUrl);
+  const pageHTML = await page.evaluate(
+    "new XMLSerializer().serializeToString(document.doctype) + document.documentElement.outerHTML"
+  );
+  await browser.close();
+  return pageHTML;
+};
+app.listen(port, () => console.log(`Image scraper app server is running on  port ${port}!..........`));
--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from "react";
-import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import InputBar from './components/InputBar'
-import Images from './components/Images'
+import React,{useState} from 'react';
+import logo from './logo.svg';
+import axios from 'axios'
+import './App.css';
 
 function App() {
+  const [text, setText] = useState('')
+  const [imageData, setImageData] = useState([])
+  const handleSubmit = async() => {
+    await axios.post(`http://localhost:3006/api/data?url=${text}`, text).then((res) => {
+      // console.log(res.data)
+      setImageData(res.data)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
   return (
-    <div className="container">
-      <InputBar/>
-      <Images/>
+    <div className="App">
+      {/* <InputBar/>
+      <ImagesList/> */}
+      <input type = "text" onChange={(e) => setText(e.target.value)}/>
+      <button onClick={handleSubmit}>Submit</button>
+
+      <div style={{margin: "10px"}}>
+        {imageData.length && imageData.map((e,idx) => (
+          <img src={e} key={idx} alt="no_image_available" style={{padding: "5px"}} height="200px" width="200px"/>
+        ))}
+      </div>
     </div>
   );
 }
